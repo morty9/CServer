8979b28e777337171ae9a78002976855af6af7e0
Bérangère La Touche <berangerelatouche@fr.ibm.com>
2018-06-07 17:42:48 +0200

    multithread in progress

--- client.c
+++ client.c
@@ -8,14 +8,15 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#define BUFFSIZE 1024 /* default size */
+#define PORT 8888 /* port number */
+
 int main(int argc, char *argv[]) {
 
 	/* VARIABLES */
 	int sockfd; /* socket */
-  int size = 1024; /* default size */
-  int port = 8888; /* port number */
   int n;
-	char buffer[size]; /* Buffer who send file */
+	char buffer[BUFFSIZE]; /* Buffer who send file */
 	struct sockaddr_in sockaddr_server; /* server addr */
 
 	/* CREATE SOCKET */
@@ -28,14 +29,14 @@ int main(int argc, char *argv[]) {
 	/* INITIALISATION OF SOCKADDR_SERVER STRUCTURE */
   bzero(&sockaddr_server, sizeof(sockaddr_server));
 	sockaddr_server.sin_family = AF_INET;
-	sockaddr_server.sin_port = htons(port);
+	sockaddr_server.sin_port = htons(PORT);
   sockaddr_server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	/* CONNECTION TO SERVER */
 	if (connect(sockfd, (struct sockaddr *)&sockaddr_server, sizeof(struct sockaddr)) == -1) {
     printf("[ERROR] Failed to connect to remote\n");
 	} else {
-    printf("[CLIENT] Connected to server on port %d\n\n", port);
+    printf("[CLIENT] Connected to server on port %d\n\n", PORT);
   }
 
   while ((n = read(sockfd, buffer, sizeof(buffer)-1)) > 0) {
--- server.c
+++ server.c
@@ -6,16 +6,22 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define BUFFSIZE 1024 /* default size */
+#define PORT 8888 /* port number */
+
+void *connection_handler(void *socket);
+
 int main(int argc, char *argv[]) {
 
   /* VARIABLES */
   int sockfd;
   int new_sockfd;
-  int size = 512;
-  int port = 8888;
+  int* new_socket;
   unsigned int sockfd_size;
   struct sockaddr_in sockaddr_client; /* client addr */
   struct sockaddr_in sockaddr_server; /* server addr */
+  char buffer[BUFFSIZE]; /* Buffer who received file */
+  char* message;
 
   /* CREATE SOCKET */
   if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ) {
@@ -28,7 +34,7 @@ int main(int argc, char *argv[]) {
   /* INITIALISATION OF SOCKADDR_CLIENT STRUCTURE */
   bzero(&sockaddr_client, sizeof(sockaddr_client));
   sockaddr_client.sin_family = AF_INET; // Protocol
-  sockaddr_client.sin_port = htons(port); // Port
+  sockaddr_client.sin_port = htons(PORT); // Port
   sockaddr_client.sin_addr.s_addr = INADDR_ANY; // Address
 
   /* BIND */
@@ -36,21 +42,37 @@ int main(int argc, char *argv[]) {
     printf("[ERROR] Failed to bind\n");
     exit(1);
   } else {
-    printf("[SERVER] Bind success, port: %d, address: 127.0.0.1\n",port);
+    printf("[SERVER] Bind success, port: %d, address: 127.0.0.1\n",PORT);
   }
 
   /* LISTEN */
   if(listen(sockfd,3) == -1) {
-    printf("[ERROR] Failed to listen on port %d\n", port);
+    printf("[ERROR] Failed to listen on port %d\n", PORT);
     exit(1);
   } else {
-    printf ("[SERVER] Listening on port %d.\n", port);
+    printf ("[SERVER] Listening on port %d.\n", PORT);
   }
 
   sockfd_size = sizeof(struct sockaddr_in);
   while((new_sockfd = accept(sockfd, (struct sockaddr *)&sockaddr_client, &sockfd_size))) {
 
     printf("[SERVER] Connection accepted for client %d\n", new_sockfd);
+
+    //Thread creation
+    pthread_t sniffer_thread;
+    new_socket = malloc(1);
+    *new_socket = new_sockfd;
+
+    //Thread connection handler
+    if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_socket) < 0){
+        printf("[SERVER] Could not create thread\n");
+        return 1;
+    }
+
+    //Now join the thread , so that we dont terminate before the thread
+    //pthread_join( sniffer_thread , NULL);
+    fflush(stdin);
+    printf("[SERVER] Handler assigned to client %d\n", new_sockfd);
 
   }
 
@@ -62,4 +84,27 @@ int main(int argc, char *argv[]) {
   close(sockfd);
   close(new_sockfd);
 
+}
+
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *socket)
+{
+    //Get the socket descriptor
+    int sock = *(int*)socket;
+    int read_size;
+    char *message;
+    char client_message[BUFFSIZE];
+
+    //Send some messages to the client
+    message = "\n[SERVER] Greetings! I am your connection handler, this is to communicate with you...\n";
+		if(write(sock , message , strlen(message)) < 0) {
+				printf("[ERROR] Failed to sent message.\n");
+		}
+
+    //Free the socket pointer
+    free(socket);
+
+    return 0;
 }
